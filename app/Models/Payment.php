3819d70bc9e6546bb5e9cdc64d348cc499973d5a
= <=>
2022-05-14 20:38:09 +0100

    Abstract month scope methodes into trait

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -2,14 +2,14 @@
 
 namespace App\Models;
 
+use App\Const\MonthRelativeTrait;
 use App\Const\RefGenerator;
-use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Payment extends Model
 {
-    use HasFactory;
+    use HasFactory, MonthRelativeTrait;
 
     protected $table = "payments";
     protected $fillable = [
@@ -29,16 +29,6 @@ class Payment extends Model
         self::creating(function ($model) {
             $model->ref = RefGenerator::generate();
         });
-    }
-
-    public function scopeCurrentMonth($query)
-    {
-        $query->where('created_at', '>', now()->subMonth());
-    }
-
-    public function scopePreviousMonth($query)
-    {
-        $query->whereBetween('created_at', [now()->subMonths(2) ,now()->subMonth()]);
     }
 
     public function student()
