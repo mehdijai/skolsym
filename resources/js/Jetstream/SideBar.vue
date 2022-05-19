36b342003f527a483bcccfbb633da10cdf46f509
= <=>
2022-05-19 20:18:32 +0100

    Fixed sidebar

--- resources/js/Jetstream/SideBar.vue
+++ resources/js/Jetstream/SideBar.vue
@@ -63,49 +63,76 @@ const logout = () => {
 };
 </script>
 <template>
-  <div class="flex flex-col bg-white dark:bg-gray-800 shadow w-fit min-h-screen">
-    <div class="flex items-center justify-between mx-4 my-4">
-      <!-- Logo -->
-      <div v-if="!collapsed" class="shrink-0 flex items-center">
-        <Link :href="route('dashboard')">
-          <JetApplicationMark
-            class="
-              hover:text-teal-500
-              transition-colors
-              duration-300
-              ease-in-out
-            "
-          />
-        </Link>
+  <div class="bg-white dark:bg-gray-800 shadow w-72">
+    <div class="fixed w-60 left-0 top-0 h-screen flex flex-col">
+      <div class="flex items-center justify-between mx-4 my-4">
+        <!-- Logo -->
+        <div v-if="!collapsed" class="shrink-0 flex items-center">
+          <Link :href="route('dashboard')">
+            <JetApplicationMark
+              class="
+                hover:text-teal-500
+                transition-colors
+                duration-300
+                ease-in-out
+              "
+            />
+          </Link>
+        </div>
+        <span
+          @click="toggleCollapse"
+          :class="{ 'mx-auto': collapsed }"
+          class="
+            material-icons
+            text-gray-800
+            bg-gray-200
+            rounded-full
+            p-3
+            w-3
+            h-3
+            text-xs
+            cursor-pointer
+            hover:bg-gray-100
+            transition
+            duration-300
+            ease-in-out
+          "
+          >{{ collapsed ? "navigate_next" : "navigate_before" }}</span
+        >
       </div>
-      <span
-        @click="toggleCollapse"
-        :class="{'mx-auto' : collapsed}"
-        class="
-          material-icons
-          text-gray-800
-          bg-gray-200
-          rounded-full
-          p-3
-          w-3
-          h-3
-          text-xs
-          cursor-pointer
-          hover:bg-gray-100
-          transition
-          duration-300
-          ease-in-out
-        "
-        >{{ collapsed ? "navigate_next" : "navigate_before" }}</span
-      >
-    </div>
-    <nav class="h-full flex flex-col gap-3 my-5 px-4">
-      <template v-for="(link, index) in navRoutes" :key="index">
+      <nav class="h-full flex flex-col gap-3 my-5 px-4">
+        <template v-for="(link, index) in navRoutes" :key="index">
+          <Link
+            :href="route(link.route)"
+            :class="{
+              'border-r-2 border-gray-600 dark:border-gray-300 text-gray-800 dark:text-gray-100  bg-gray-100 dark:bg-gray-600':
+                route().current(link.route.split('.')[0] + '.*'),
+            }"
+            class="
+              hover:text-gray-800 hover:bg-gray-100
+              flex
+              items-center
+              p-2
+              transition-colors
+              dark:hover:text-white dark:hover:bg-gray-600
+              duration-200
+              text-gray-500
+              dark:text-gray-400
+            "
+          >
+            <span class="material-icons text-sm">{{ link.icon }}</span>
+            <span v-if="!collapsed" class="mx-4 font-medium">
+              {{ link.name }}
+            </span>
+            <span class="flex-grow text-right"> </span>
+          </Link>
+        </template>
+        <hr class="mt-auto" />
         <Link
-          :href="route(link.route)"
+          :href="route('profile.show')"
           :class="{
             'border-r-2 border-gray-600 dark:border-gray-300 text-gray-800 dark:text-gray-100  bg-gray-100 dark:bg-gray-600':
-              route().current(link.route.split('.')[0] + '.*'),
+              route().current('profile.show'),
           }"
           class="
             hover:text-gray-800 hover:bg-gray-100
@@ -119,53 +146,30 @@ const logout = () => {
             dark:text-gray-400
           "
         >
-          <span class="material-icons text-sm">{{ link.icon }}</span>
-          <span v-if="!collapsed" class="mx-4 font-medium"> {{ link.name }} </span>
+          <span class="material-icons text-sm">manage_accounts</span>
+          <span v-if="!collapsed" class="mx-4 font-medium">Profile</span>
           <span class="flex-grow text-right"> </span>
         </Link>
-      </template>
-      <hr class="mt-auto">
-      <Link
-        :href="route('profile.show')"
-        :class="{
-          'border-r-2 border-gray-600 dark:border-gray-300 text-gray-800 dark:text-gray-100  bg-gray-100 dark:bg-gray-600':
-            route().current('profile.show'),
-        }"
-        class="
-          hover:text-gray-800 hover:bg-gray-100
-          flex
-          items-center
-          p-2
-          transition-colors
-          dark:hover:text-white dark:hover:bg-gray-600
-          duration-200
-          text-gray-500
-          dark:text-gray-400
-        "
-      >
-        <span class="material-icons text-sm">manage_accounts</span>
-        <span v-if="!collapsed" class="mx-4 font-medium">Profile</span>
-        <span class="flex-grow text-right"> </span>
-      </Link>
 
-      <button
-        @click="logout"
-        class="
-          hover:text-gray-800 hover:bg-gray-100
-          flex
-          items-center
-          p-2
-          transition-colors
-          dark:hover:text-white dark:hover:bg-gray-600
-          duration-200
-          text-gray-500
-          dark:text-gray-400
-        "
-      >
-        <span class="material-icons text-sm">exit_to_app</span>
-        <span v-if="!collapsed" class="mx-4 font-medium">Log out</span>
-        <span class="flex-grow text-right"> </span>
-      </button>
-    </nav>
+        <button
+          @click="logout"
+          class="
+            hover:text-gray-800 hover:bg-gray-100
+            flex
+            items-center
+            p-2
+            transition-colors
+            dark:hover:text-white dark:hover:bg-gray-600
+            duration-200
+            text-gray-500
+            dark:text-gray-400
+          "
+        >
+          <span class="material-icons text-sm">exit_to_app</span>
+          <span v-if="!collapsed" class="mx-4 font-medium">Log out</span>
+          <span class="flex-grow text-right"> </span>
+        </button>
+      </nav>
+    </div>
   </div>
 </template>
