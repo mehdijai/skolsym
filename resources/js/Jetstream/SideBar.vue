d82dce51863edae377aa04e7cb13e21e903b4429
= <=>
2022-05-30 21:16:36 +0100

    refactor layout and sidebar

--- resources/js/Jetstream/SideBar.vue
+++ resources/js/Jetstream/SideBar.vue
@@ -63,76 +63,49 @@ const logout = () => {
 };
 </script>
 <template>
-  <div class="bg-white dark:bg-gray-800 shadow w-72">
-    <div class="fixed w-60 left-0 top-0 h-screen flex flex-col">
-      <div class="flex items-center justify-between mx-4 my-4">
-        <!-- Logo -->
-        <div v-if="!collapsed" class="shrink-0 flex items-center">
-          <Link :href="route('dashboard')">
-            <JetApplicationMark
-              class="
-                hover:text-teal-500
-                transition-colors
-                duration-300
-                ease-in-out
-              "
-            />
-          </Link>
-        </div>
-        <span
-          @click="toggleCollapse"
-          :class="{ 'mx-auto': collapsed }"
-          class="
-            material-icons
-            text-gray-800
-            bg-gray-200
-            rounded-full
-            p-3
-            w-3
-            h-3
-            text-xs
-            cursor-pointer
-            hover:bg-gray-100
-            transition
-            duration-300
-            ease-in-out
-          "
-          >{{ collapsed ? "navigate_next" : "navigate_before" }}</span
-        >
-      </div>
-      <nav class="h-full flex flex-col gap-3 my-5 px-4">
-        <template v-for="(link, index) in navRoutes" :key="index">
-          <Link
-            :href="route(link.route)"
-            :class="{
-              'border-r-2 border-gray-600 dark:border-gray-300 text-gray-800 dark:text-gray-100  bg-gray-100 dark:bg-gray-600':
-                route().current(link.route.split('.')[0] + '.*'),
-            }"
+  <div class="bg-white h-full w-full flex flex-col">
+    <div class="flex items-center justify-between mx-4 my-4">
+      <!-- Logo -->
+      <div v-if="!collapsed" class="shrink-0 flex items-center">
+        <Link :href="route('dashboard')">
+          <JetApplicationMark
             class="
-              hover:text-gray-800 hover:bg-gray-100
-              flex
-              items-center
-              p-2
+              hover:text-teal-500
               transition-colors
-              dark:hover:text-white dark:hover:bg-gray-600
-              duration-200
-              text-gray-500
-              dark:text-gray-400
+              duration-300
+              ease-in-out
             "
-          >
-            <span class="material-icons text-sm">{{ link.icon }}</span>
-            <span v-if="!collapsed" class="mx-4 font-medium">
-              {{ link.name }}
-            </span>
-            <span class="flex-grow text-right"> </span>
-          </Link>
-        </template>
-        <hr class="mt-auto" />
+          />
+        </Link>
+      </div>
+      <span
+        @click="toggleCollapse"
+        :class="{ 'mx-auto': collapsed }"
+        class="
+          material-icons
+          text-gray-800
+          bg-gray-200
+          rounded-full
+          p-3
+          w-3
+          h-3
+          text-xs
+          cursor-pointer
+          hover:bg-gray-100
+          transition
+          duration-300
+          ease-in-out
+        "
+        >{{ collapsed ? "navigate_next" : "navigate_before" }}</span
+      >
+    </div>
+    <nav class="h-full flex flex-col gap-3 my-5 px-4">
+      <template v-for="(link, index) in navRoutes" :key="index">
         <Link
-          :href="route('profile.show')"
+          :href="route(link.route)"
           :class="{
             'border-r-2 border-gray-600 dark:border-gray-300 text-gray-800 dark:text-gray-100  bg-gray-100 dark:bg-gray-600':
-              route().current('profile.show'),
+              route().current(link.route.split('.')[0] + '.*'),
           }"
           class="
             hover:text-gray-800 hover:bg-gray-100
@@ -146,30 +119,55 @@ const logout = () => {
             dark:text-gray-400
           "
         >
-          <span class="material-icons text-sm">manage_accounts</span>
-          <span v-if="!collapsed" class="mx-4 font-medium">Profile</span>
+          <span class="material-icons text-sm">{{ link.icon }}</span>
+          <span v-if="!collapsed" class="mx-4 font-medium">
+            {{ link.name }}
+          </span>
           <span class="flex-grow text-right"> </span>
         </Link>
+      </template>
+      <hr class="mt-auto" />
+      <Link
+        :href="route('profile.show')"
+        :class="{
+          'border-r-2 border-gray-600 dark:border-gray-300 text-gray-800 dark:text-gray-100  bg-gray-100 dark:bg-gray-600':
+            route().current('profile.show'),
+        }"
+        class="
+          hover:text-gray-800 hover:bg-gray-100
+          flex
+          items-center
+          p-2
+          transition-colors
+          dark:hover:text-white dark:hover:bg-gray-600
+          duration-200
+          text-gray-500
+          dark:text-gray-400
+        "
+      >
+        <span class="material-icons text-sm">manage_accounts</span>
+        <span v-if="!collapsed" class="mx-4 font-medium">Profile</span>
+        <span class="flex-grow text-right"> </span>
+      </Link>
 
-        <button
-          @click="logout"
-          class="
-            hover:text-gray-800 hover:bg-gray-100
-            flex
-            items-center
-            p-2
-            transition-colors
-            dark:hover:text-white dark:hover:bg-gray-600
-            duration-200
-            text-gray-500
-            dark:text-gray-400
-          "
-        >
-          <span class="material-icons text-sm">exit_to_app</span>
-          <span v-if="!collapsed" class="mx-4 font-medium">Log out</span>
-          <span class="flex-grow text-right"> </span>
-        </button>
-      </nav>
-    </div>
+      <button
+        @click="logout"
+        class="
+          hover:text-gray-800 hover:bg-gray-100
+          flex
+          items-center
+          p-2
+          transition-colors
+          dark:hover:text-white dark:hover:bg-gray-600
+          duration-200
+          text-gray-500
+          dark:text-gray-400
+        "
+      >
+        <span class="material-icons text-sm">exit_to_app</span>
+        <span v-if="!collapsed" class="mx-4 font-medium">Log out</span>
+        <span class="flex-grow text-right"> </span>
+      </button>
+    </nav>
   </div>
 </template>
--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -26,12 +26,12 @@ onMounted(() => {
 
     <JetBanner />
 
-    <div class="min-h-screen h-full bg-gray-100 flex flex-column">
-      <SideBar />
-
-      <div class="w-full">
-        <!-- Page Heading -->
-        <header v-if="$slots.header" class="bg-white shadow">
+    <div class="app-body">
+      <aside class="side-navigation shadow-md">
+        <SideBar />
+      </aside>
+      <section class="app-content bg-gray-100">
+        <header v-if="$slots.header" class="app-header bg-white shadow">
           <div class="max-w-7xl mx-auto flex items-center justify-between">
             <div class="py-2 px-4 sm:px-6 lg:px-8">
               <slot name="header" />
@@ -41,13 +41,37 @@ onMounted(() => {
             </div>
           </div>
         </header>
-
-        <!-- Page Content -->
-        <main>
+        <main class="app-slot">
           <slot />
         </main>
-      </div>
+      </section>
     </div>
   </div>
 </template>
 
+<style lang="scss">
+.app-body {
+  width: 100%;
+  display: flex;
+  align-items: stretch;
+  height: 100vh;
+  .side-navigation {
+    width: fit;
+    height: 100%;
+    z-index: 10;
+  }
+  .app-content {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    .app-header {
+      z-index: 2;
+    }
+    .app-slot {
+      overflow-y: scroll;
+      height: 100%;
+    }
+  }
+}
+</style>
